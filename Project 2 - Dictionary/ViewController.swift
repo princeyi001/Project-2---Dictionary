a09620e65ab5569663b240a55f396d8c7749999f
Dong Hun Yi <dhyi@Dongs-MacBook.local>
2017-09-06 18:31:02 -0400

    Tableview and Working on Second Screen

--- Project 2 - Dictionary/ViewController.swift
+++ Project 2 - Dictionary/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableview1: UITableView!
+    
+    var emoji = ["😃","😡","😱","😵","😈","👹","💩"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableview1.dataSource = self
+        tableview1.delegate = self
+        
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emoji.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emoji[indexPath.row]
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
